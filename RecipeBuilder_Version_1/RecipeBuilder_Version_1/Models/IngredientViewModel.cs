3dab188c5e07e42551c1a51b4bd3b966118c40ab
Chef-Code <lonnieteter@yahoo.com>
2016-01-21 23:01:53 -0800

    Add project Recipe Builder
    Basic application that uses entity frameworks

---
+++ RecipeBuilder_Version_1/RecipeBuilder_Version_1/Models/IngredientViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeBuilder_Version_1.Models
+{
+    public class IngredientViewModel
+    {
+        public int IngredientID { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string Preparation { get; set; }
+
+        public Recipe RecipeItem { get; set; }  //This is just a reference, it is not the whole container
+    }
+}
---
+++ RecipeBuilder_Version_1/RecipeBuilder_Version_1/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecipeBuilder_Version_1.Models
+{
+    public class Menu
+    {
+        public int MenuID { get; set; }
+        List<Recipe> recipes = new List<Recipe>();
+
+        public List<Recipe> Recipes { get { return recipes; } }
+    }
+}
